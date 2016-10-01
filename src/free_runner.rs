92f537401513544b26470ef09cb6ddd77482711d
Gustav Behm <gustav.behm@gmail.com>
2016-10-01 17:17:52 +0200

    Send heartbeats to the runner

--- src/free_runner.rs
+++ src/free_runner.rs
@@ -1,9 +1,11 @@
 extern crate time;
+extern crate schedule_recv;
 
 use std::thread;
 use self::time::{Tm, now_utc};
 use std::thread::JoinHandle;
-use std::sync::mpsc::{channel, Sender};
+use std::sync::mpsc::{channel, Sender, SendError};
+use std::time::Duration;
 
 pub enum Event<E> {
     Heartbeat { time: Tm },
@@ -18,7 +20,7 @@ pub enum Effect<E, T> {
 pub fn run<Ev: Send + 'static, Eff, T: Send + 'static, F, G>(f: F, g: G) -> (Sender<Event<Ev>>, JoinHandle<T>)
     where F: Fn(Event<Ev>) -> Option<Effect<Eff, T>> + Send + 'static, G: Fn(Eff) -> Option<Ev> + Send + 'static {
         let (tx, rx) = channel();
-        let tx_copy = tx.clone();
+        let tx_clone = tx.clone();
         let t = thread::spawn(move || {
             loop {
                 let ev = rx.recv().unwrap();
@@ -32,7 +34,8 @@ pub fn run<Ev: Send + 'static, Eff, T: Send + 'static, F, G>(f: F, g: G) -> (Sen
                 }
             }
         });
-        (tx_copy, t)
+
+        (tx_clone, t)
     }
 
 pub fn effect<Eff, T>(eff: Eff) -> Option<Effect<Eff, T>> {
@@ -51,10 +54,30 @@ pub fn event<E>(e: E) -> Option<E> {
     Some(e)
 }
 
+pub fn send<Ev>(s: Sender<Event<Ev>>, ev: Ev) -> Result<(), SendError<Event<Ev>>> {
+    s.send(Event::Event { time: now_utc(), event: ev })
+}
+
+pub fn heartbeats<Ev: Send + 'static>(s: Sender<Event<Ev>>, duration: Duration) -> thread::JoinHandle<()> {
+    thread::spawn(move || {
+        let rx = schedule_recv::periodic(duration);
+        loop {
+            match rx.recv() {
+                Ok(_) => match s.send(Event::Heartbeat { time: now_utc() }) {
+                    Ok(_) => (),
+                    Err(_) => return ()
+                },
+                Err(e) => panic!("{}", e),
+            }
+        }
+    })
+}
+
 #[cfg(test)]
 mod test {
     extern crate time;
     use free_runner::*;
+    use std::time::Duration;
 
     enum TestEvent {
         Foo(u32),
@@ -67,7 +90,7 @@ mod test {
     }
 
     #[test]
-    fn complete_an_event_into_effect_into_event_loop() {
+    fn complete_an_event_into_effect_into_event_loop_test() {
         let f = |e| match e {
             Event::Event { time: _, event: TestEvent::Foo(7) } => effect(TestEffect::Increment(7)),
             Event::Event { time: _, event: TestEvent::Foo(8) } => return_(13),
@@ -77,9 +100,19 @@ mod test {
             TestEffect::Increment(i) => event(TestEvent::Foo(i+1)),
             _ => noop(),
         };
-        let (tx, t) = run(f, g);
-        tx.send(Event::Event { time: time::now_utc(), event: TestEvent::Foo(7) }).unwrap();
+        let (sender, t) = run(f, g);
+        send(sender, TestEvent::Foo(7)).unwrap();
         assert_eq!(t.join().unwrap(), 13)
+    }
+
+    #[test]
+    fn receive_heartbeat_test() {
+        let f = |e| match e { Event::Heartbeat { time: _ } => return_(true), _ => noop() };
+        let g = |_: ()| noop::<()>();
+        let (s, t) = run(f, g);
+        let t2 = heartbeats(s, Duration::from_millis(100));
+        assert!(t.join().unwrap());
+        assert_eq!(t2.join().unwrap(), ())
     }
 }
 
