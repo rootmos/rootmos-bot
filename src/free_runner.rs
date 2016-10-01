520a3f4a0004b0658f57b5ed4207c1cf89e19d2a
Gustav Behm <gustav.behm@gmail.com>
2016-10-01 17:54:01 +0200

    Wrap the runner methods into a struct

--- src/free_runner.rs
+++ src/free_runner.rs
@@ -6,6 +6,7 @@ use self::time::{Tm, now_utc};
 use std::thread::JoinHandle;
 use std::sync::mpsc::{channel, Sender, SendError};
 use std::time::Duration;
+use std::any::Any;
 
 pub enum Event<E> {
     Heartbeat { time: Tm },
@@ -17,8 +18,16 @@ pub enum Effect<E, T> {
     Effect(E),
 }
 
-pub fn run<Ev: Send + 'static, Eff, T: Send + 'static, F, G>(f: F, g: G) -> (Sender<Event<Ev>>, JoinHandle<T>)
-    where F: Fn(Event<Ev>) -> Option<Effect<Eff, T>> + Send + 'static, G: Fn(Eff) -> Option<Ev> + Send + 'static {
+pub struct Runner<Ev, T> {
+    sender: Sender<Event<Ev>>,
+    join_handle: JoinHandle<T>,
+    heartbeats_join_handle: Option<JoinHandle<()>>,
+}
+
+impl <Ev, T> Runner<Ev, T> {
+    pub fn new<F, G, Eff>(f: F, g: G) -> Runner<Ev, T>
+        where F: Fn(Event<Ev>) -> Option<Effect<Eff, T>> + Send + 'static, G: Fn(Eff) -> Option<Ev> + Send + 'static, T: Send + 'static, Ev: Send + 'static {
+
         let (tx, rx) = channel();
         let tx_clone = tx.clone();
         let t = thread::spawn(move || {
@@ -35,8 +44,39 @@ pub fn run<Ev: Send + 'static, Eff, T: Send + 'static, F, G>(f: F, g: G) -> (Sen
             }
         });
 
-        (tx_clone, t)
+        Runner { sender: tx_clone, join_handle: t, heartbeats_join_handle: None }
     }
+
+    pub fn join(self) -> Result<T, Box<Any + Send + 'static>> {
+        match self.heartbeats_join_handle {
+            Some(t) => assert_eq!(t.join().unwrap(), ()),
+            None => ()
+        };
+        self.join_handle.join()
+    }
+
+    pub fn send(&self, ev: Ev) -> Result<(), SendError<Event<Ev>>> {
+        self.sender.send(Event::Event { time: now_utc(), event: ev })
+    }
+
+    pub fn heartbeats(&mut self, duration: Duration) -> () where Ev: Send + 'static {
+        assert!(self.heartbeats_join_handle.is_none());
+
+        let cloned_sender = self.sender.clone();
+        self.heartbeats_join_handle = Some(thread::spawn(move || {
+            let rx = schedule_recv::periodic(duration);
+            loop {
+                match rx.recv() {
+                    Ok(_) => match cloned_sender.send(Event::Heartbeat { time: now_utc() }) {
+                        Ok(_) => (),
+                        Err(_) => return ()
+                    },
+                    Err(e) => panic!("{}", e),
+                }
+            }
+        }))
+    }
+}
 
 pub fn effect<Eff, T>(eff: Eff) -> Option<Effect<Eff, T>> {
     Some(Effect::Effect(eff))
@@ -54,24 +94,6 @@ pub fn event<E>(e: E) -> Option<E> {
     Some(e)
 }
 
-pub fn send<Ev>(s: Sender<Event<Ev>>, ev: Ev) -> Result<(), SendError<Event<Ev>>> {
-    s.send(Event::Event { time: now_utc(), event: ev })
-}
-
-pub fn heartbeats<Ev: Send + 'static>(s: Sender<Event<Ev>>, duration: Duration) -> thread::JoinHandle<()> {
-    thread::spawn(move || {
-        let rx = schedule_recv::periodic(duration);
-        loop {
-            match rx.recv() {
-                Ok(_) => match s.send(Event::Heartbeat { time: now_utc() }) {
-                    Ok(_) => (),
-                    Err(_) => return ()
-                },
-                Err(e) => panic!("{}", e),
-            }
-        }
-    })
-}
 
 #[cfg(test)]
 mod test {
@@ -100,19 +122,18 @@ mod test {
             TestEffect::Increment(i) => event(TestEvent::Foo(i+1)),
             _ => noop(),
         };
-        let (sender, t) = run(f, g);
-        send(sender, TestEvent::Foo(7)).unwrap();
-        assert_eq!(t.join().unwrap(), 13)
+        let runner = Runner::new(f, g);
+        runner.send(TestEvent::Foo(7)).unwrap();
+        assert_eq!(runner.join().unwrap(), 13)
     }
 
     #[test]
     fn receive_heartbeat_test() {
         let f = |e| match e { Event::Heartbeat { time: _ } => return_(true), _ => noop() };
         let g = |_: ()| noop::<()>();
-        let (s, t) = run(f, g);
-        let t2 = heartbeats(s, Duration::from_millis(100));
-        assert!(t.join().unwrap());
-        assert_eq!(t2.join().unwrap(), ())
+        let mut runner = Runner::new(f, g);
+        runner.heartbeats(Duration::from_millis(100));
+        assert!(runner.join().unwrap());
     }
 }
 
